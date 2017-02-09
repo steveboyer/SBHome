9b0cf6116db65f17e5e64800d47b01d7234178f8
Stephen Boyer <sboyer307@gmail.com>
2017-02-08 19:45:18 -0700

    Base working

--- hello/views.py
+++ hello/views.py
@@ -4,12 +4,12 @@ import requests
 from .models import Greeting
 
 # Create your views here.
-def index(request):    
-	r = requests.get('http://httpbin.org/status/418')
-	print r.text
-	return HttpResponse('Hello from Python!')
-    	#return render(request, 'index.html')
-	
+def index(request):
+    r = requests.get('http://httpbin.org/status/418')
+    print r.text
+    return render(request, 'index.html')
+
+#return HttpResponse('Hello from Pythonhhhhhh!')
 
 def db(request):
 
