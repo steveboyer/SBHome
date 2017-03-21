6a0b258eeaefaf208fa005099439c82dd81435aa
Stephen Boyer <sboyer307@gmail.com>
2017-03-21 13:52:39 -0600

    Config

--- manage.py
+++ manage.py
@@ -4,7 +4,3 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stephenboyer.settings")
-
-    from django.core.management import execute_from_command_line
-
-    execute_from_command_line(sys.argv)
