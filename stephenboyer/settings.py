02d4495c46be69440e34bfce91a9eca901348fa2
Stephen Boyer <sboyer307@gmail.com>
2017-03-21 13:41:52 -0600

    Config change

--- stephenboyer/settings.py
+++ stephenboyer/settings.py
@@ -79,11 +79,11 @@ WSGI_APPLICATION = 'stephenboyer.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'stephenboyer',
-        'USER': 'stephenboyeruser',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '',
+        'NAME': 'dboilnuodcuo2j',
+        'USER': 'ndhkfhjnytnxli',
+        'PASSWORD': '600b096fd053355d663816bd4accbe6b7fd79bfff4e1c56ffa3dbf69eaae3fde',
+        'HOST': 'ec2-54-225-67-3.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
